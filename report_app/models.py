dc2c4a0f819d98a3456cdcd506019b4d432dd5b6
Joshua Malemba <jm850@student.le.ac.uk>
2023-06-02 11:02:59 +0100

    create models and migration file

--- report_app/models.py
+++ report_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField()
+    vendor_id = models.IntegerField()
+    customer_id = models.IntegerField()
+
+
+class OrderLine(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_id = models.IntegerField()
+    product_description = models.CharField(max_length=255)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_vat_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    discount_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.IntegerField()
+    full_price_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    discounted_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    vat_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=255)
+
+
+class Promotion(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=255)
+
+
+class ProductPromotion(models.Model):
+    date = models.DateField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    promotion = models.ForeignKey(Promotion, on_delete=models.CASCADE)
+
+
+class VendorCommissions(models.Model):
+    date = models.DateField()
+    vendor_id = models.IntegerField()
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
